89ce4dd3699dee27be1756d2a879f7bbf7887430
Raffaello Bonghi <raffaello@rnext.it>
2016-10-10 14:59:26 +0200

    Added publisher

--- src/example_unav_gpio_node.cpp
+++ src/example_unav_gpio_node.cpp
@@ -2,6 +2,20 @@
 #include "std_msgs/String.h"
 
 #include <sstream>
+#include <orbus_interface/Peripheral.h>
+
+orbus_interface::Peripheral msg_send;
+
+/**
+ * Subscriber GPIO messages
+ */
+void chatterCallback(const orbus_interface::Peripheral::ConstPtr& msg)
+{
+  if(msg.get()->gpio.at(0) == 1)
+  {
+      ROS_INFO_STREAM("High!");
+  }
+}
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -9,4 +23,13 @@
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "talker");
+  // Init Node handle
+  ros::NodeHandle n;
+
+  ros::Subscriber sub_gpio = n.subscribe("unav/peripheral", 1000, chatterCallback);
+  ros::Publisher pub_gpio = n.advertise<orbus_interface::Peripheral>("unav/cmd_peripheral", 1000);
+
+  ros::spin();
+
+  return 0;
 }
